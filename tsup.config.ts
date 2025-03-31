79904b00910480cdeed1e65e9ad499d88c6f99bd
cje <chej098t@gmail.com>
2025-04-01 01:08:43 +0900

    chore: change folder structure

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/extension.ts"],
+  entry: ["packages/extension/index.ts"],
   format: ["cjs"],
   shims: false,
   dts: false,
